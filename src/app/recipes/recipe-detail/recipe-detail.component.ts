5fe94f383ebc569f5adcdd0634ab48d0d90600a6
Morgan Hondros <morganhondros@gmail.com>
2021-01-21 23:16:32 -0500

    Ingredients added to recipes. Fucntionality added to put ingredients from Recipes to Shopping List.

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,3 +1,4 @@
+import { RecipeService } from '../recipe.service';
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
@@ -5,14 +6,16 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.css']
+  styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
-
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,27 +1,39 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       'A Test Recipe',
       'This is simply a test.',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [new Ingredient('Celery', 4), new Ingredient('Soy Sauce', 3)]
     ),
     new Recipe(
       'A Second Test Recipe',
       'Another test recipe to see if this is working.',
-      'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'
+      'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/master/pass/Basically-Gojuchang-Chicken-Recipe-Wide.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'Sesame Chicken',
       'Ive always loved sesame chicken.',
-      'https://www.dinneratthezoo.com/wp-content/uploads/2015/04/sesame-chicken-1.jpg'
+      'https://www.dinneratthezoo.com/wp-content/uploads/2015/04/sesame-chicken-1.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
